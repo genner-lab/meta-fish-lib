5a99881959587d93e1c4b9b57824191cfc003953
rupert@jau <rupertcollins@gmail.com>
2021-01-06 22:47:58 +0000

    added functions script

---
+++ scripts/funs.R
@@ -0,0 +1,7 @@
+#!/usr/bin/env Rscript
+
+# https://rstudio.github.io/renv/index.html
+# initialise renv, install packages and update
+# renv::init()
+# renv::install(packages=c("rfishbase","tidyverse","vroom","rentrez","bold","traits"))
+# renv::update()
