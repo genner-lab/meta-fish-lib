6128bd3b7883ff1145edaaede9ca15d8389d40df
rupert@jau <rupertcollins@gmail.com>
2021-01-07 20:39:15 +0000

    added more packages and funs

--- scripts/load-libs.R
+++ scripts/load-libs.R
@@ -3,7 +3,7 @@
 # https://rstudio.github.io/renv/index.html
 # initialise renv, install packages, update, snapshot to lock file
 # renv::init()
-# renv::install(packages=c("tidyverse","vroom","rentrez","bold","traits","rfishbase","ape"))
+# renv::install(packages=c("tidyverse","magrittr","lubridate","vroom","rentrez","bold","traits","rfishbase","ape","ips","phangorn","spider"))
 # renv::update()
 # renv::snapshot()
 
@@ -11,12 +11,16 @@ suppressMessages({
     library("parallel")
     library("tidyverse")
     library("magrittr")
+    library("lubridate")
     library("vroom")
     library("rentrez")
     library("bold")
     library("traits")
     library("rfishbase")
     library("ape")
+    library("ips")
+    library("phangorn")
+    library("spider")
 })
 
 
@@ -140,4 +144,100 @@ run_hmmer3 <- function(dir, infile, hmm, prefix, evalue, coords){#
     stop("Please provide 'env' or 'ali' as arguments to coords")
     }
     return(mtdna.sub.coords)
+}
+
+
+# FUN TO SUBSET A REFERENCE LIB FOR EACH MARKER
+subset_nucs <- function(pref,df){
+    df %<>% rename(nucleotidesFrag=!!as.name(paste0("nucleotidesFrag.",pref)), lengthFrag=!!as.name(paste0("lengthFrag.",pref)))
+    df %<>% filter(!is.na(nucleotidesFrag))
+    return(df)
+}
+
+
+# FUNCTION TO CALCULATE SPECIES THAT DROP OUT OF A DATASET AFTER LENGTH TRIMMING
+# species_lost(df=reflib,thresh=0.5)
+# threshold is a proportion of the mean sequence length
+species_lost <- function(df,thresh){
+    removed <- df %>% filter(lengthFrag < (median(lengthFrag)*thresh)) %>% select(sciNameValid)
+    kept <- df %>% filter(lengthFrag >= (median(lengthFrag)*thresh)) %>% select(sciNameValid)
+    tot <- setdiff(removed$sciNameValid, kept$sciNameValid)
+    return(tot)
+}
+
+
+# FUNCTION TO CALCULATE SEQUENCES REMOVED FROM A DATASET AFTER LENGTH TRIMMING
+# sequences_removed(df=reflib,thresh=0.5)
+# threshold is a proportion of the mean sequence length
+sequences_removed <- function(df,thresh){
+    removed <- df %>% filter(lengthFrag < (median(lengthFrag)*thresh)) %>% select(dbid)
+    n.removed <- length(removed$dbid)
+    return(n.removed)
+}
+
+
+# COLLAPSES HAPLOTYPES (FROM A DATAFRAME FORMAT TO A DATAFRAME FORMAT)
+# need to specify columns that contain sequence lengths, and nucleotides
+# hap_collapse_df(df=mydataframe,lengthcol="lengthFrag",nuccol="nucleotidesFrag")
+# add a number of each haplotype
+hap_collapse_df <- function(df,lengthcol,nuccol,cores){
+    odf <- df[order(df[[lengthcol]],decreasing=TRUE),]
+    reps <- mcmapply(FUN=function(x) which(str_detect(string=odf[[nuccol]], pattern=x) == TRUE)[1], odf[[nuccol]], SIMPLIFY=TRUE, USE.NAMES=FALSE, mc.cores=cores)
+    ind <- unique(reps)
+    dat <- odf[ind,]
+    dat[["nHaps"]] <- as.numeric(table(reps))
+    return(dat)
+}
+
+
+# FUNCTION TO GET RETRIEVE SPECIES NAMES OF SEQUENCES WITH AN IDENTICAL HAPLOTYPE AS YOUR QUERY 
+# works on a dataframe
+# get_sames(df=mydataframe,ids="dbid",nucs="nucleotidesFrag",sppVec="sciNameValid",query=mydataframe$nucleotidesFrag[[1]])
+get_sames <- function(df,ids,nucs,sppVec,query){
+    per.ind <- df[[sppVec]][str_detect(df[[nucs]], query)]
+    return(per.ind)
+}
+
+
+# FUN TO ANNOTATE A REFERENCE LIBRARY TABLE WITH NUMBER HAPLOTYPES PER SPECIES
+haps2fas <- function(df){
+    df <- bind_rows(mcmapply(FUN=function(x) hap_collapse_df(df=x,lengthcol="lengthFrag",nuccol="nucleotidesFrag",cores=1), split(df,pull(df,sciNameValid)), SIMPLIFY=FALSE,mc.cores=1))
+    sames <- mclapply(FUN=function(x) get_sames(df=df,ids="dbid",nucs="nucleotidesFrag",sppVec="sciNameValid",query=x), pull(df,nucleotidesFrag), mc.cores=1)
+    df %<>% mutate(nMatches=sapply(sames, function(x) length(unique(x))), matchTax=sapply(sames, function(x) paste(unique(x),collapse=" | ")))
+    df %<>% mutate(noms=paste(dbid,str_replace_all(sciNameValid," ","_"),nHaps,sep="|")) %>% arrange(class,order,family,genus,sciNameValid,lengthFrag,dbid)
+    return(df)
+}
+
+
+# FUN TO ALIGN SEQS AND MAKE A PHYLOGENTIC TREE
+phylogenize <- function(fas,prefix,binLoc,version){
+    fas <- ips::mafft(fas,exec="mafft",method="retree 2",maxiterate=2)
+    tr <- ips::raxml(fas, file=paste0("fromR-",prefix), m="GTRCAT", f="d", p=42, exec=binLoc, N=1)
+    tr <- tr$bestTree
+    tmp.path <- paste0("temp/qc_v",version,"_",paste(month(ymd(Sys.Date()),label=TRUE),year(ymd(Sys.Date())),sep="-"))
+    dir.create(path=tmp.path)
+    flist <- list.files(pattern=prefix)
+    file.copy(flist, paste0(tmp.path,"/",flist))
+    file.remove(flist)
+    write.tree(tr,file=paste0(tmp.path,"/",prefix,".nwk"))
+    return(tr)
+}
+
+
+# FUN TO PLOT AND ANNOTATE PHYLOGENETIC TREES
+plot_trees <- function(tr,df,prefix,version){
+    tr <- ape::ladderize(phangorn::midpoint(tr))
+    sppv <- pull(df,sciNameValid)[match(str_split_fixed(tr$tip.label,"\\|",3)[,1],pull(df,dbid))]
+    monov <- spider::monophyly(tr,sppVector=sppv)
+    allmono <- monov[match(sppv, unique(sppv))]
+    cols <- rep("gray20",length(tr$tip.label))
+    cols[which(allmono==FALSE)] <- "hotpink"
+    cols[match(df$noms[which(df$nMatches>1)], tr$tip.label)] <- "green3"
+    tmp.path <- paste0("temp/qc_v",version,"_",paste(month(ymd(Sys.Date()),label=TRUE),year(ymd(Sys.Date())),sep="-"))
+    dfs <- df %>% summarise(nSeqs=sum(nHaps),nHaps=length(nHaps),nSpp=length(unique(sciNameValid)))
+    tit <- paste0(str_replace_all(prefix,"\\.noprimers",""),"\n(n=",pull(dfs,nSeqs),", n haps=",pull(dfs,nHaps),", n spp.=",pull(dfs,nSpp),")\npink = non-monophyletic species\ngreen = shared haplotypes\nscroll down for tree ...")
+    pdf(file=paste0(tmp.path,"/RAxML_bestTree.",prefix,".pdf"), width=15, height=length(tr$tip.label)/10)
+    plot.phylo(tr, tip.col=cols, cex=0.5, font=1, label.offset=0.01, no.margin=TRUE)
+    title(tit, line=-10)
+    dev.off()
 }
