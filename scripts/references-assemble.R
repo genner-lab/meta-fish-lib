da372b7be1bb408b2af5afb00d6c620208e04dc0
rupert@jau <rupertcollins@gmail.com>
2021-01-07 18:57:47 +0000

    added working scripts

---
+++ scripts/references-assemble.R
@@ -0,0 +1,182 @@
+#!/usr/bin/env Rscript
+
+# R script to make reference databases for UK fishes for multiple markers
+# then, it uses a hidden markov model to pull out the fragment of interest from all that mtDNA data
+# then, it queries NCBI/BOLD for those accessions, and retrieves full metadata for them to allow better curation of reference database
+# output is a csv dataframe of all accessions with data for all primer sets if present
+
+## Load functions and libs
+source("scripts/load-libs.R")
+# set cores - mc.cores=1 is the safest option, but try extra cores to speed up if there are no errors
+cores <- 2
+
+
+## Data
+# load up the uk species table
+species.table <- suppressMessages(read_csv(file="assets/species-table.csv"))
+# load the BOLD dump
+bold.red <- suppressMessages(read_csv(file="temp/bold-dump.csv", guess_max=100000))
+
+
+## Extract the frag of interest using the HMMs
+# now run hmmer
+# need to have "hmmer" and "biosquid" installed 
+# if not, run 'sudo apt install hmmer biosquid'
+# assumes the hidden markov model is located in assets/hmms directory and is named '$prefix.hmm'
+# returns a DNAbin object of the sequences matched by hmmer 
+
+prefixes.all <- c(
+    "coi.lerayxt.noprimers",
+    "coi.ward.noprimers",
+    "12s.miya.noprimers",
+    "12s.riaz.noprimers",
+    "12s.valentini.noprimers",
+    "12s.taberlet.noprimers",
+    "16s.berry.noprimers",
+    "cytb.minamoto.noprimers"
+)
+
+# run hmmer (takes about 5 mins)
+writeLines("\nExtracting metabarcode fragments with HMMER ...")
+dat.frag.all <- lapply(prefixes.all, function(x) run_hmmer3(dir="temp", infile="mtdna-dump.fas", prefix=x, evalue="10", coords="env"))
+writeLines("\nDone")
+
+# concatentate all
+dat.frag.cat <- do.call(c,dat.frag.all)
+
+# get unique names
+dat.frag.names <- unique(labels(dat.frag.cat))
+
+# separate the extracted sequences that are in GenBank or BOLD
+in.bold <- dat.frag.names[dat.frag.names %in% bold.red$processidUniq]
+in.gb <- dat.frag.names[!dat.frag.names %in% bold.red$processidUniq]
+
+# now for the same sequences, get the tabular data from NCBI using 'ncbi_byid' to make a proper reference database
+chunk <- 70
+chunk.frag <- unname(split(in.gb, ceiling(seq_along(in.gb)/chunk)))
+writeLines("\nRetrieving metadata from NCBI ...\n")
+ncbi.frag <- mcmapply(FUN=ncbi_byid, chunk.frag, SIMPLIFY=FALSE, USE.NAMES=FALSE, mc.cores=cores)
+
+# check for errors (should all be "data.frame")
+if(length(sapply(ncbi.frag,class)) == length(sapply(ncbi.frag,class) == "data.frame")) {
+    writeLines("\nNCBI metadata sucessfully retrieved")
+    } else {writeLines("\nNCBI search failed, try again")}
+
+# join all the data sets
+writeLines("\nNow cleaning data ...")
+frag.df <- as_tibble(bind_rows(ncbi.frag))
+
+# from GenBank remove ncbi genome and other duplicates etc, and clean the lat/lon data
+frag.df %<>% filter(gi_no!="NCBI_GENOMES") %>% 
+    distinct(gi_no, .keep_all=TRUE) %>% 
+    mutate(acc_no=str_replace_all(acc_no,"\\.[0-9]",""), source="GENBANK") %>%
+    # fix the lat_lon into decimal
+    mutate(lat=paste(str_split_fixed(lat_lon, " ", 4)[,1], str_split_fixed(lat_lon, " ", 4)[,2]), lon=paste(str_split_fixed(lat_lon, " ", 4)[,3], str_split_fixed(lat_lon, " ", 4)[,4])) %>%
+    mutate(lat=if_else(grepl(" N",lat), true=str_replace_all(lat," N",""), false=if_else(grepl(" S",lat), true=paste0("-",str_replace_all(lat," S","")), false=lat))) %>%
+    mutate(lon=if_else(grepl(" E",lon), true=str_replace_all(lon," E",""), false=if_else(grepl(" W",lon), true=paste0("-",str_replace_all(lon," W","")), false=lon))) %>% 
+    mutate(lat=str_replace_all(lat,"^ ", NA_character_), lon=str_replace_all(lon,"^ ", NA_character_)) %>%
+    mutate(lat=as.numeric(lat), lon=as.numeric(lon)) %>% 
+    # tidy up
+    select(-taxonomy,-organelle,-keyword,-lat_lon) %>% 
+    rename(sciNameOrig=taxon,notesGenBank=gene_desc,dbid=gi_no,gbAccession=acc_no,catalogNumber=specimen_voucher,
+    publishedAs=paper_title,publishedIn=journal,publishedBy=first_author,date=uploaded_date,decimalLatitude=lat,decimalLongitude=lon,nucleotides=sequence)
+
+# do the same for BOLD
+# run
+bold.red %<>% filter(processidUniq %in% in.bold) %>% 
+    filter(!genbank_accession %in% frag.df$gbAccession) %>% 
+    mutate(source="BOLD",nucleotides=str_to_lower(nucleotides), length=as.character(str_length(nucleotides))) %>% 
+    select(source,processidUniq,genbank_accession,species_name,lat,lon,country,institution_storing,catalognum,nucleotides,length) %>%
+    rename(dbid=processidUniq,gbAccession=genbank_accession,sciNameOrig=species_name,decimalLatitude=lat,decimalLongitude=lon,institutionCode=institution_storing,catalogNumber=catalognum)
+
+# merge gb and bold data
+dbs.merged.all <- bind_rows(frag.df,bold.red)
+
+# name each DNAbin object
+names(dat.frag.all) <- prefixes.all
+
+# extract nucleotides out of the DNAbin objects
+dat.frag.flat <- lapply(dat.frag.all, function(x) mcmapply(str_flatten, as.character(x), mc.cores=cores, SIMPLIFY=TRUE,USE.NAMES=TRUE))
+
+# turn each into a dataframe
+dat.frag.df <- lapply(dat.frag.flat, function(x) tibble(names=names(x), seqs=unlist(x), lengthFrag=str_length(seqs)))
+
+# rename each df with names of the fragment
+dat.frag.df <- mapply(function(x,y,z) dplyr::rename(x,dbid=names, !!y:=seqs, !!z:=lengthFrag), dat.frag.df, paste("nucleotidesFrag",names(dat.frag.df),sep="."), paste("lengthFrag",names(dat.frag.df),sep="."), SIMPLIFY=FALSE)
+
+# merge all the data frames 
+dat.frag.merged <- dat.frag.df %>% purrr::reduce(full_join, by="dbid")
+
+# join with the metadata dataframe
+dbs.merged.all <- dplyr::left_join(dbs.merged.all,dat.frag.merged,by="dbid")
+
+
+## Get the proper fishbase taxonomy, not the NCBI nonsense
+
+# make a binomial scientific name - clean mess
+dbs.merged.all %<>% mutate(sciNameBinomen=apply(str_split_fixed(sciNameOrig, " ", 3)[,1:2], 1, paste, collapse=" "))
+
+# get up to date spp list and taxonomy
+fishbase.synonyms <- rfishbase::synonyms(server="fishbase")
+# clean up fishbase - just accepted names and synonyms
+fishbase.synonyms.acc <- fishbase.synonyms %>% mutate(TaxonLevel=str_replace_all(TaxonLevel,"^species","Species")) %>% filter(Status=="accepted name" & TaxonLevel=="Species")
+fishbase.synonyms.syn <- fishbase.synonyms %>% mutate(Status=str_replace_all(Status,"Synonym","synonym")) %>% filter(Status=="synonym")
+
+# make ref of valid uk species 
+uk.species.valid <- species.table %>% select(fbSpecCode,validName,class,order,family,genus,commonName) %>% distinct()
+
+# annotate with fishbase codes and valid species names
+dbs.merged.all %<>% mutate(fbSpecCode=pull(fishbase.synonyms.acc,SpecCode)[match(sciNameBinomen,pull(fishbase.synonyms.acc,synonym))]) %>% 
+    mutate(fbSpecCode=if_else(is.na(fbSpecCode),pull(fishbase.synonyms.syn,SpecCode)[match(sciNameBinomen,pull(fishbase.synonyms.syn,synonym))],fbSpecCode)) %>%
+    mutate(sciNameValid=pull(uk.species.valid,validName)[match(fbSpecCode,pull(uk.species.valid,fbSpecCode))])
+
+# drop missing taxa
+missing <- dbs.merged.all %>% filter(is.na(sciNameValid)) %>% pull(sciNameOrig) %>% unique()
+writeLines(paste("\nThe following taxa could not be found in UK fishes database and have been dropped:",paste(missing,collapse=", ")))
+dbs.merged.all %<>% filter(!is.na(sciNameValid))
+
+# print all the species that had their names updated
+writeLines("\nThe following taxa had their GenBank names updated using FishBase:")
+dbs.merged.all %>% filter(sciNameOrig != sciNameValid) %>% select(sciNameOrig,sciNameValid) %>% arrange(sciNameOrig) %>% distinct() %>% print(n=Inf)
+
+# add taxonomy
+dbs.merged.all %<>% mutate(subphylum="Vertebrata",
+    class=pull(uk.species.valid,class)[match(fbSpecCode,pull(uk.species.valid,fbSpecCode))],
+    order=pull(uk.species.valid,order)[match(fbSpecCode,pull(uk.species.valid,fbSpecCode))],
+    family=pull(uk.species.valid,family)[match(fbSpecCode,pull(uk.species.valid,fbSpecCode))],
+    genus=pull(uk.species.valid,genus)[match(fbSpecCode,pull(uk.species.valid,fbSpecCode))])
+
+# clean up nucs
+dbs.merged.all %<>% mutate(nucleotides=str_to_lower(nucleotides))
+
+
+## Clean up to make the table human readable
+
+# drop the DNA fragments and reorder the columns 
+dbs.merged.info <- dbs.merged.all %>% select(-matches("Frag")) %>% 
+   select(source,dbid,gbAccession,sciNameValid,subphylum,class,order,family,genus,sciNameBinomen,sciNameOrig,fbSpecCode,
+    country,catalogNumber,institutionCode,decimalLatitude,decimalLongitude,publishedAs,publishedIn,publishedBy,
+    date,notesGenBank,length,nucleotides)
+
+# make a data frame of just the sequence data
+dbs.merged.seqs <- dbs.merged.all %>% select(matches("Frag|dbid"))
+
+# remerge with the reorganised dataframe and remove any ids with no nucleotides
+dbs.merged.final <- left_join(dbs.merged.info,dbs.merged.seqs,by="dbid") %>%
+    arrange(class,order,family,genus,sciNameValid) %>% 
+    filter(!is.na(nucleotides))
+
+# take a look 
+writeLines("\nReference library fields:")
+glimpse(dbs.merged.final)
+
+## Write out
+# first, see what was added/removed from last time
+#old <- read_csv("../references/uk-fish-references.csv.gz")
+#old %>% filter(!dbid %in% pull(dbs.merged.final,dbid)) %>% filter(source=="GENBANK") %>% select(dbid,sciNameOrig,sciNameBinomen,sciNameValid) %>% print(n=Inf)
+#dbs.merged.final %>% filter(!dbid %in% pull(old,dbid)) %>% select(dbid,sciNameOrig,sciNameBinomen,sciNameValid) %>% print(n=Inf)
+
+# write out a gzipped file (orig is too big for github)
+writeLines("\nWriting out reference library to 'reports/reference-library-master.csv.gz' ...")
+write_csv(dbs.merged.final, file=gzfile("reports/reference-library-master.csv.gz"), na="")
+writeLines("\nAll operations completed!\nPlease read previous messages in case of error\n")
---
+++ scripts/sequences-download.R
@@ -0,0 +1,160 @@
+#!/usr/bin/env Rscript
+# Rupert A. Collins
+
+# R script to make reference databases for UK fishes for multiple markers
+# downloads all mtDNA sequence data from GenBank/BOLD, for a provided list of species 
+
+# load functions and libs
+source("scripts/load-libs.R")
+# load up your personal NCBI API key to get 10 requests per sec. This needs to be generated from your account at https://www.ncbi.nlm.nih.gov/
+# DO NOT PUT THIS KEY ON GITHUB
+# if you don't have one, ncbi will rate-limit your access to 3 requests per sec, and errors may occur.
+source("assets/ncbi-key.R")
+
+
+# load up the species table
+species.table <- suppressMessages(read_csv(file="assets/species-table.csv"))
+# report
+writeLines(paste0("\nSpecies table contains ",length(pull(species.table,speciesName))," species names"))
+
+# check the GenBank data release number against the record of previous download
+gb.version <- read.table("ftp://ftp.ncbi.nih.gov/genbank/GB_Release_Number")$V1
+writeLines(paste0("\nGenBank is at version ",gb.version))
+
+### Download all GenBank sequences for species in species table (including synonyms) with mtDNA
+# make a query for genbank
+range <- "40:20000" # includes mt genomes, no bigger
+gene.syns <- c("COI","12S","16S","rRNA","ribosomal","cytb","CO1","cox1","cytochrome","subunit","COB","CYB","mitochondrial","mitochondrion")
+spp.list <- unique(c(pull(species.table,speciesName),pull(species.table,validName)))
+query <- unlist(mapply(function(x) paste0("(",spp.list,"[ORGN] AND ",x,"[ALL] AND ",range,"[SLEN])"), gene.syns, SIMPLIFY=FALSE, USE.NAMES=FALSE))
+
+# randomise the query
+set.seed(42)
+query <- sample(query,length(query))
+query <- query[1:100]
+
+# set n cores to parallel search in n threads
+# cores=1 is the safest option, but more cores are faster if there are no errors
+# do not try more than 10 cores (with api key)
+# do not try more than 3 cores (without api key)
+# important - try to run the search when server loads are lowest, i.e. at weekends or when the USA is not at work.
+# should take about 1.5 h with 4 cores
+cores <- 4
+
+# break up into chunks
+# longest query should be no larger than about 2500 chars - reduce chunk.size to get smaller queries
+chunk.size.rentrez <- 35
+query.split  <- split(query, ceiling(seq_along(query)/chunk.size.rentrez))
+
+# collapse into strings of n species per string
+query.cat <- unname(sapply(query.split, paste, collapse=" OR "))
+
+# chunk queries over the n cores
+queries.chunked  <- split(query.cat, ceiling(seq_along(query.cat)/cores))
+
+# run NCBI search and time
+writeLines(paste("\nNow searching GenBank on",cores,"cores ...\n"))
+    start_time <- Sys.time()
+search.res <- lapply(queries.chunked,entrez_search_parallel,threads=cores,key=ncbi.key)
+    end_time <- Sys.time()
+    end_time-start_time
+
+# check for errors - should be all false
+if(TRUE %in% grepl("Error",search.res)) {
+    writeLines("\nSome of the searches failed, try again with fewer cores or when the USA is not online")
+    } else {writeLines("\nAll search batches successful")
+}
+
+# flatten the searches
+search.flat <- search.res %>% purrr::flatten()
+
+# get the ids out the the non-zero results
+search.ids <- search.flat[which(search.flat %>% purrr::map(~{unname(.x$count)}) > 0)] %>% purrr::map(~{unname(.x$ids)}) %>% purrr::flatten_chr() %>% unique()
+
+
+### now download ids using ape
+# chunk up into 70s to stop server from rejecting request 
+chunk.size.ape <- 70
+id.split <- unname(split(search.ids, ceiling(seq_along(search.ids)/chunk.size.ape)))
+
+# download with modifed ape function (fast)
+writeLines(paste("\nNow downloading",length(search.ids),"sequences from GenBank ..."))
+ncbi.all <- mcmapply(FUN=function(x) read_GenBank(x, species.names=FALSE, api.key=ncbi.key), id.split, SIMPLIFY=FALSE, USE.NAMES=FALSE, mc.cores=cores)
+
+# check for errors (should be all DNAbin)
+if(length(sapply(ncbi.all, class)) == length(sapply(ncbi.all, class) == "DNAbin")) {
+    writeLines("\nAll sequences sucessfully downloaded")
+    } else {writeLines("\nDownload failed, try again with fewer cores or when the USA is not online")}
+
+# write out a temporary file
+writeLines("\nWriting out in FASTA format ...")
+suppressMessages({
+    file.create("temp/mtdna-dump.fas")
+    invisible(lapply(ncbi.all, write.FASTA, file="temp/mtdna-dump.fas", append=TRUE))
+})
+
+
+### Now repeat the same for the BOLD database
+
+writeLines("\nNow searching BOLD ...")
+
+# chunk up the BOLD requests
+chunk.size.bold <- 70
+bold.split <- unname(split(spp.list, ceiling(seq_along(spp.list)/chunk.size.bold)))
+bold.split <- bold.split[1:4]
+
+# query BOLD and retrieve a table
+# sometimes an error occurs, just run again
+bold.all <- mcmapply(FUN=function(x) bold_seqspec(x,format="tsv",sepfasta=FALSE,response=FALSE), bold.split, SIMPLIFY=FALSE, USE.NAMES=FALSE, mc.cores=cores)
+
+# check for errors (should be "data.frame" or "logical", not "character")
+if(length(sapply(bold.all, class)) == length(sapply(bold.all, class) == "data.frame")) {
+    writeLines("\nBOLD results sucessfully retrieved")
+    } else {writeLines("\nBOLD search failed, try again")}
+
+# remove the non-dataframes
+bold.all <- bold.all[which(sapply(bold.all, class)=="data.frame")]
+
+# tidy it up and join it together, remove duplicate records
+bold.red <- lapply(lapply(bold.all, as_tibble), function(x) mutate_all(x,as.character))
+bold.red <- bind_rows(bold.red)
+bold.red %<>% 
+    mutate(nucleotides=str_replace_all(nucleotides,"-",""), nucleotides=str_replace_all(nucleotides,"N",""), num_bases=nchar(nucleotides)) %>% 
+    filter(num_bases > 0) %>%
+    filter(institution_storing!="Mined from GenBank, NCBI") %>% 
+    mutate(processidUniq=paste(processid,markercode,sep=".")) %>% 
+    distinct(processidUniq, .keep_all=TRUE)
+
+# write temp copy of the bold dump
+write_csv(bold.red,file="temp/bold-dump.csv")
+
+# create a fasta file of BOLD
+bold.fas <- tab2fas(df=bold.red,seqcol="nucleotides",namecol="processidUniq")
+
+# add it to the GenBank file already created
+write.FASTA(bold.fas, file="temp/mtdna-dump.fas", append=TRUE)
+
+
+### report a summary table
+stats <- tibble(
+    stat=c("speciesTotal","speciesValid","speciesSynonyms","genbankVersion","queriesUnique","queriesMerged","queriesPerMerge","queriesMergedPerBatch","batches","maxRecordsPerMerge","totalRecordsGenbank","totalRecordsBold"),
+    n=c(species.table %>% distinct(speciesName) %>% nrow(),
+        species.table %>% filter(status == "accepted name") %>% distinct(speciesName) %>% nrow(),
+        species.table %>% filter(status != "accepted name") %>% distinct(speciesName) %>% nrow(),
+        gb.version,
+        length(query),
+        length(query.cat),
+        chunk.size.rentrez,
+        cores,
+        length(queries.chunked),
+        search.flat %>% purrr::map(~{unname(.x$count)}) %>% purrr::flatten_int() %>% max(),
+        length(search.ids),
+        length(pull(bold.red,processidUniq))
+        )
+)
+
+# print and save
+writeLines("\nPrinting stats ...\n")
+print(stats,n=Inf)
+write_csv(stats,file="temp/stats.csv")
+writeLines("\nAll operations completed!\nPlease read previous messages in case of error\n")
