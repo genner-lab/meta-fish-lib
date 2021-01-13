48555e8ef248baefa0745fd8da02141d5347a36f
rupert@jau <rupertcollins@gmail.com>
2021-01-13 01:20:08 +0000

    added version output

--- scripts/references-load-remote.R
+++ scripts/references-load-remote.R
@@ -5,11 +5,14 @@ start_time <- Sys.time()
 writeLines("\nLoading reference library remotely...")
 # load up the species info table
 uk.species.table <- vroom::vroom("https://raw.githubusercontent.com/genner-lab/meta-fish-lib/main/assets/species-table.csv",delim=",",num_threads=1,guess_max=99999,col_types=cols())
-# load up the reference library
+# load up the reference library and get version and dates
 reflib.orig <- vroom::vroom("https://github.com/genner-lab/meta-fish-lib/raw/main/assets/reference-library-master.csv.gz",delim=",",num_threads=1,guess_max=99999,col_types=cols())
+gbv <- reflib.orig %>% dplyr::select(genbankVersion) %>% tidyr::drop_na() %>% dplyr::distinct(genbankVersion) %>% dplyr::pull(genbankVersion)
+gbd <- reflib.orig %>% dplyr::select(searchDate) %>% tidyr::drop_na() %>% dplyr::distinct(searchDate) %>% dplyr::pull(searchDate)
 # load exclusions file
 exclusions <- vroom::vroom("https://raw.githubusercontent.com/genner-lab/meta-fish-lib/main/assets/exclusions.csv",delim=",",num_threads=1,guess_max=99999,col_types=cols())
 # write encouraging words
 end_time <- Sys.time()
 writeLines(paste(dim(reflib.orig)[1], "reference sequences loaded in", round(end_time-start_time,digits=1), "seconds"))
+writeLines(paste0("\nGenBank version ", gbv, ", searched ",gbd))
 writeLines("\nReference library objects are now in your memory")
