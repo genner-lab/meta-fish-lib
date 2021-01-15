41907b75ddd8cf757fea98f3f6cff846ea6069b0
rupert@jau <rupertcollins@gmail.com>
2021-01-15 15:18:26 +0000

    added label description

--- scripts/load-libs.R
+++ scripts/load-libs.R
@@ -239,7 +239,7 @@ plot_trees <- function(tr,df,prefix,version){
     tmp.path <- paste0("reports/qc_v",version,"_",paste(month(ymd(Sys.Date()),label=TRUE),year(ymd(Sys.Date())),sep="-"))
     dir.create(path=tmp.path)
     dfs <- df %>% summarise(nSeqs=sum(nHaps),nHaps=length(nHaps),nSpp=length(unique(sciNameValid)))
-    tit <- paste0(str_replace_all(prefix,"\\.noprimers",""),"\n(n=",pull(dfs,nSeqs),", n haps=",pull(dfs,nHaps),", n spp.=",pull(dfs,nSpp),")\npink = non-monophyletic species\ngreen = shared haplotypes\nscroll down for tree ...")
+    tit <- paste0(str_replace_all(prefix,"\\.noprimers",""),"\n(n=",pull(dfs,nSeqs),", n haps=",pull(dfs,nHaps),", n spp.=",pull(dfs,nSpp),")\nlabel format = 'dbid|genus species|n haplotypes'\npink = non-monophyletic species\ngreen = shared haplotypes\nscroll down for tree ...")
     pdf(file=paste0(tmp.path,"/RAxML_bestTree.",prefix,".pdf"), width=15, height=length(tr$tip.label)/10)
     plot.phylo(tr, tip.col=cols, cex=0.5, font=1, label.offset=0.01, no.margin=TRUE)
     title(tit, line=-10)
