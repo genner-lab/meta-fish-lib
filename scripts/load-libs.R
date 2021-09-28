3c98dd500713ab69c6469c88071edf9dee5cdd12
rc16041@it031740 <rupertcollins@gmail.com>
2021-09-28 15:18:19 +0100

    fixed colon problem

--- scripts/load-libs.R
+++ scripts/load-libs.R
@@ -225,7 +225,7 @@ haps2fas <- function(df){
     df <- bind_rows(mcmapply(FUN=function(x) hap_collapse_df(df=x,lengthcol="lengthFrag",nuccol="nucleotidesFrag",cores=1), split(df,pull(df,sciNameValid)), SIMPLIFY=FALSE,mc.cores=1))
     sames <- mclapply(FUN=function(x) get_sames(df=df,ids="dbid",nucs="nucleotidesFrag",sppVec="sciNameValid",query=x), pull(df,nucleotidesFrag), mc.cores=1)
     df %<>% mutate(nMatches=sapply(sames, function(x) length(unique(x))), matchTax=sapply(sames, function(x) paste(unique(x),collapse=" | ")))
-    df %<>% mutate(noms=paste(dbid,str_replace_all(sciNameValid," ","_"),nHaps,sep="|")) %>% arrange(class,order,family,genus,sciNameValid,lengthFrag,dbid)
+    df %<>% mutate(noms=paste(dbid,str_replace_all(sciNameValid," |:","_"),nHaps,sep="|")) %>% arrange(class,order,family,genus,sciNameValid,lengthFrag,dbid)
     return(df)
 }
 
