5a259c1e3126ee2bbdfa489a736a5e949734a941
rupert@jau <rupertcollins@gmail.com>
2023-05-22 21:21:05 +0100

    changed back to raxml-ng lh-epsilon defaults after upgrade to v1.2.0

--- scripts/load-libs.R
+++ scripts/load-libs.R
@@ -287,7 +287,7 @@ raxml_ng <- function(file,verbose) {
         system(command=string.mafft,ignore.stdout=FALSE)
         string.parse <- paste0("raxml-ng --parse --msa ",file,".ali --model TN93+F+G --seed 42 --redo --threads auto")
         system(command=string.parse,ignore.stdout=FALSE)
-        string.search <- paste0("raxml-ng --search --msa ",file,".ali.raxml.rba --tree pars{1} --lh-epsilon 10 --seed 42 --redo --threads auto")
+        string.search <- paste0("raxml-ng --search --msa ",file,".ali.raxml.rba --tree pars{1} --seed 42 --redo --threads auto")#--lh-epsilon 10 
         system(command=string.search,ignore.stdout=FALSE)
         rax.tr <- ape::read.tree(file=paste0(file,".ali.raxml.rba.raxml.bestTree"))
     } else if (verbose == "false") {
@@ -295,7 +295,7 @@ raxml_ng <- function(file,verbose) {
         system(command=string.mafft,ignore.stdout=FALSE)
         string.parse <- paste0("raxml-ng --parse --msa ",file,".ali --model TN93+F+G --seed 42 --redo --threads auto")
         system(command=string.parse,ignore.stdout=TRUE)
-        string.search <- paste0("raxml-ng --search --msa ",file,".ali.raxml.rba --tree pars{1} --lh-epsilon 10 --seed 42 --redo --threads auto")
+        string.search <- paste0("raxml-ng --search --msa ",file,".ali.raxml.rba --tree pars{1} --seed 42 --redo --threads auto")#--lh-epsilon 10 
         system(command=string.search,ignore.stdout=TRUE)
         rax.tr <- ape::read.tree(file=paste0(file,".ali.raxml.rba.raxml.bestTree"))
     } else stop(writeLines("'-v' value must be 'true' or 'false'."))
